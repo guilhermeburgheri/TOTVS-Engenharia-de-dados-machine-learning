b4fb365c49f992b4b448a0a2ce7f07d594c0069e
Guilherme Burgheri <guilherme.burgheri@hotmail.com>
2026-04-07 13:09:14 -0300

    Aulas 6 e 7.
    Métodos para trabalhar com listas e tuplas.

---
+++ Aula07/Tuplas.py
@@ -0,0 +1,92 @@
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
+
+
+frutas = ("maçã", "laranja", "uva", "pera",)
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+
+frutas = ("maçã", "laranja", "uva", "pera",)
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+print(matriz[0])  # (1, "a", 2)
+print(matriz[0][0])  # 1
+print(matriz[0][-1])  # 2
+print(matriz[-1][-1])  # "c"
+
+
+#FATIAR
+tupla = ("p", "y", "t", "h", "o", "n",)
+
+print(tupla[2:])  # ("t", "h", "o", "n")
+print(tupla[:2])  # ("p", "y")
+print(tupla[1:3])  # ("y", "t")
+print(tupla[0:3:2])  # ("p", "t")
+print(tupla[::])  # ("p", "y", "t", "h", "o", "n")
+print(tupla[::-1])  # ("n", "o", "h", "t", "y", "p")
+
+
+#ITERAR
+carros = (
+    "gol",
+    "celta",
+    "palio",
+)
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+
+#COUNT
+cores = (
+    "vermelho",
+    "azul",
+    "verde",
+    "azul",
+)
+
+print(cores.count("vermelho"))  # 1
+print(cores.count("azul"))  # 2
+print(cores.count("verde"))  # 1
+
+
+#INDEX
+linguagens = ("python", "js", "c", "java", "csharp",)
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("python"))  # 0
+
+
+#LEN
+linguagens = ("python", "js", "c", "java", "csharp",)
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("python"))  # 0
